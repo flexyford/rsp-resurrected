d438f2bf3f9dc2cdd01e476de158e6e6d239ad10
Flexy Ford <flexondatrack@gmail.com>
2014-12-18 09:10:08 -0600

    Add initial skeletons for all endpoints

--- server.rb
+++ server.rb
@@ -3,7 +3,7 @@ require 'sinatra/reloader'
 require 'json'
 require 'pry-byebug'
 
-require './lib/RockPaperScissors.rb'
+require './lib/rockPaperScissors.rb'
 
 # set :bind, '0.0.0.0' # This is needed for Vagrant
 
@@ -59,6 +59,10 @@ class RockPaperScissors::Server < Sinatra::Application
     send_file 'public/index.html'
   end
 
+  # POST /signup : This endpoint is for signing up for an account with RockPaperScissors.
+  #     Params : `username` - The username of the user signing in
+  #              `password` - The password of the user signing in
+  #    Returns : An empty 200 response.
   post '/signup' do
     errors = []
     if !params[:password] || params[:password] == ''
@@ -82,7 +86,10 @@ class RockPaperScissors::Server < Sinatra::Application
       { errors: errors }.to_json
     end
   end
-
+  # POST /signin : This endpoint accepts two parameters for the sign in process.
+  #     Params : `username` - The username of the user signing in
+  #              `password` - The password of the user signing in
+  #    Returns : An `apiToken` to make future authorized requests with.
   post '/signin' do
     user = RockPaperScissors::UsersRepo.find_by_name db, params[:username]
 
@@ -93,7 +100,9 @@ class RockPaperScissors::Server < Sinatra::Application
       status 401
     end
   end
-
+  # DELETE /signout : This endpoint accepts two parameters for the sign in process.
+  #     Params : `apiToken` - The username of the user signing in
+  #    Returns : An empty 200 response.
   delete '/signout' do
     RockPaperScissors::UsersRepo.sign_out db, params[:apiToken]
     status 200
@@ -103,39 +112,90 @@ class RockPaperScissors::Server < Sinatra::Application
   ##########################################
   # event stream stuff.
 
-  #get '/chats' do
-  #  if params[:since]
-  #    messages.select { |m| params[:since] < m['time'] }
-  #  else
-  #    messages.last 10
-  #  end.to_json
-  #end
- #
- # post '/chats' do
- #   if @current_user
- #
- #     msg = params[:message]
- #     if msg.nil? || msg == ''
- #       status 400
- #       return { errors: ["blank_message"] }.to_json
- #     elsif messages.find {|m| m[:message] == msg }
- #       status 400
- #       return { errors: ["message_already_exists"] }.to_json
- #     end
+  # GET  /users  : This endpoint will allow you to receive users from the server.
+  #     Params : None.
+  #     Return : An Array of Hashes containing user_data
+  GET '/users' do
 
- #     message_count += 1
- #     messages << {
- #       user: @current_user['username'],
- #       message: msg,
- #       time: timestamp,
- #       id: message_count
- #     }
- #     status 200
- #     '{}'
- #   else
- #     status 403
- #     { errors: "invalid_api_key" }.to_json
- #   end
- # end
+  end
+
+  # GET  /users/:user_id  : This endpoint will allow you to receive users from the server.
+  #     Params : None.
+  #     Return : An Hash containing user_data for user_id
+  GET '/users/:user_id' do
+
+  end
+
+  # GET  /users/:user_id/matches  : This endpoint will allow you to receive users from the server.
+  #     Params : None.
+  #     Return : An Array of Hashes containing matches_data for user_id
+  GET '/users/:user_id/matches' do
+
+  end
+
+  # GET  /users/:user_id/matches  : This endpoint will allow you to receive users from the server.
+  #     Params : None.
+  #     Return : An Array of Hashes containing rounds_data for user_id
+  GET '/users/:user_id/rounds' do
+
+  end
+
+  # GET  /matches  : This endpoint will allow you to receive matches from the server.
+  #     Params : None.
+  #     Return : An Array of Hashes containing match_data
+  GET '/matches' do
+
+  end
+
+  # POST  /matches  : This endpoint will allow you to create a new match and new round for that match
+  #     Params : `apiToken` - The api token of the signed in user.
+  #              `guest_name` - The username of the challenged guest
+  #     Return : A Hash containing { 'match_id' => match_id, 'round_id' => round_id}
+  POST '/matches' do
+
+  end
+
+  # GET  /matches/:match_id  : This endpoint will allow you to receive match_data for match_id from the server.
+  #     Params : None.
+  #     Return : A Hash containing match_data for match_id
+  GET '/matches/:match_id' do
+
+  end
+
+  # PUT  /matches:match_id  : This endpoint will allow you to update the winner of match_id on the server.
+  #     Params : `winner_name` - The username of the decalred winner; Expecting Host or Guest of Match
+  #     Return : A Hash containing match_data for match_id
+  PUT '/matches/:match_id' do
+
+  end
+
+  # DELETE  /matches/:match_id  : This endpoint will delete a match and all rounds for that match
+  #     Params : `apiToken` - The api token of the signed in user; Must be Host or Guest of Match
+  #     Returns : An empty 200 response.
+  DELETE '/matches/:match_id' do
+
+  end
+
+  # GET  /rounds  : This endpoint will allow you to receive rounds from the server.
+  #     Params : None.
+  #     Return : An Array of Hashes containing round_data
+  GET '/rounds' do
+
+  end
+
+  # GET  /rounds:round_id  : This endpoint will allow you to receive round_data for round_id from the server.
+  #     Params : None.
+  #     Return : A Hash containing round_data for round_id
+  GET '/rounds/:round_id' do
+
+  end
+
+  # PUT  /rounds:round_id  : This endpoint will allow you to update round_data for round_id on the server.
+  #     Params : `apiToken` - The api token of the signed in user; Expecting Host or Guest of Match
+  #              'user_choice' - The choice that the currently signed-in user makes
+  #     Return : A Hash containing round_data for round_id (Winner is updated server-side)
+  PUT '/rounds/:round_id' do
+
+  end
 
 end
